d6d36d220435b99300bc42faeb50f884c53dfa6d
John Tribolet <john@tribolet.info>
2016-07-12 16:29:17 -0400

    Add/remove Home menu item when mobile menu is toggled.

--- js/apa.js
+++ js/apa.js
@@ -1,6 +1,13 @@
 (function( $ ) {
     $(function() {
         $('.menu-icon').click(function() {
+            var $homeItem = $('.menu .home');
+            if ($homeItem.length) {
+                $homeItem.remove();
+            }
+            else {
+                jQuery('.menu').prepend('<li class="leaf home"><a href="/" title="">Home</a></li>');
+            }
             $('#header').toggle();
         });
     });
